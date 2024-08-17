e77899c274fda224dd03a0c1cd6ea6e0e351cb0e
Josue <josue.corroc@gmail.com>
2024-08-16 22:51:36 -0600

    first commit

---
+++ timeline-config.js
@@ -0,0 +1,261 @@
+var timelineData = {
+    "title": {
+        "media": {
+            "url": "./resources/england.png",
+            "caption": "",
+            "credit": ""
+        },
+        "text": {
+            "headline": "Evolución del Idioma Inglés",
+            "text": "<p>Una línea del tiempo de los eventos más importantes en la evolución del idioma inglés.</p>"
+        },
+        "background": {
+            "color": "#000000",
+            "url": "./resources/intro.jpg"
+        }
+    },
+    "events": [
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/8hhoF15XPBo",
+                "caption": "Inglés Antiguo",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "450"
+            },
+            "text": {
+                "headline": "Inglés Antiguo",
+                "text": "El inglés antiguo se desarrolla a partir de las lenguas germánicas traídas a Gran Bretaña por los anglos, sajones y jutos. La lengua germánica hablada por estos pueblos evolucionó hasta convertirse en lo que conocemos como Inglés Antiguo (Old English)"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://ancientlanguage.com/wp-content/uploads/2022/09/1920px-Bayeux_Tapestry_scene29-30-31_Harold_coronation-1024x454.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://youtube.com/embed/hB5BxZjI_24",
+                "caption": "Beowulf",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "700"
+            },
+            "text": {
+                "headline": "Beowulf",
+                "text": "Se escribe el poema épico Beowulf, uno de los textos más importantes en inglés antiguo. Narra la historia de un heroico personaje que se sobrepone a obstaculos apuntando hacia la gloria. Gracias a este escrito se puede profundizar en los valores, creencias y lenguaje de los habitantes de las islas británicas en el siglo VIII."
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://miro.medium.com/v2/resize:fit:1400/1*36EBaTIP8XCWZ2R-T5F8cw.jpeg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/Gdkk8zkdYkw",
+                "caption": "William the Conqueror",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1066"
+            },
+            "text": {
+                "headline": "Conquista Normanda",
+                "text": "La conquista normanda de Inglaterra introduce una gran cantidad de vocabulario francés al inglés. Esto marca el comienzo de la evolución del inglés medio (Middle English). A pesar de que el inglés antiguo se mantuvo como lengua hablada, el francés se convirtió en la lengua de la corte y la administración."
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://www.english-heritage.org.uk/siteassets/home/learn/story-of-england/medieval/stories/atoning-for-the-bloodshed/battle-hastings-painting.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/aQq5FmOqsS0",
+                "caption": "Geoffrey Chaucer",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1387"
+            },
+            "text": {
+                "headline": "Los Cuentos de Canterbury",
+                "text": "Geoffrey Chaucer comienza a escribir 'Los Cuentos de Canterbury', una obra fundamental del inglés medio. Chaucer es reconocido por su contribución a la estandarización y embellecimiento del inglés medio, lo que lo posiciona como una figura clave en la evolución del idioma."
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://sites.krieger.jhu.edu/chaucer/files/2022/06/canterbury-tales-mural-library-of-congress-e1612958413366-scaled.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/v4cbL1XmjCI",
+                "caption": "Guerra de las Rosas",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1455"
+            },
+            "end_date": {
+                "year": "1487"
+            },
+            "text": {
+                "headline": "Guerra de las Rosas",
+                "text": "La Guerra de las Rosas fue una serie de conflictos civiles en Inglaterra entre las casas de Lancaster y York. Estos eventos tuvieron un impacto significativo en la sociedad y la cultura inglesa, incluyendo el idioma. Tras el conflicto, la victoria de la Casa de Tudor (con Enrique VII) llevó a una mayor centralización del poder en la monarquía. La corte real se convirtió en un centro de influencia cultural y lingüística. El inglés comenzó a ser usado más ampliamente en la administración y en la corte, desplazando aún más al francés normando y al latín"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://i0.wp.com/the-past.com/wp-content/uploads/2023/05/post-1_image0-72.jpg?w=1230&ssl=1"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/yI7boxKTZGI",
+                "caption": "William Caxton",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1476"
+            },
+            "text": {
+                "headline": "Introducción de la imprenta a Inglaterra por William Caxton",
+                "text": "William Caxton introduce la imprenta en Inglaterra en 1476, lo que facilita la difusión de libros y escritos en inglés. Antes de la imprenta, el inglés se escribía de muchas maneras diferentes, con gran variabilidad en la ortografía y gramática según la región y el autor. La imprenta de Caxton ayudó a estandarizar la ortografía y la gramática del inglés, ya que los textos impresos podían reproducirse de manera uniforme."
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://cdn.britannica.com/88/172688-050-C087B758/William-Caxton-Edward-IV-printing-press-page.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/skJivaVmFHk",
+                "caption": "William Shakespeare",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1600"
+            },
+            "text": {
+                "headline": "William Shakespeare",
+                "text": "<p>William Shakespeare escribe sus obras, que tienen un impacto duradero en el idioma inglés moderno. Shakespeare introdujo una enorme cantidad de palabras nuevas al inglés, muchas de las cuales siguen en uso hoy. Se le atribuye la creación o popularización de más de 1,700 palabras.</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://www.lavanguardia.com/files/og_thumbnail/uploads/2019/04/26/5fa5172fc8ddf.jpeg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/J0Dydqw2zyA",
+                "caption": "Biblia del Rey Jacobo",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1611"
+            },
+            "text": {
+                "headline": "Biblia del Rey Jacobo",
+                "text": "<p>Se publica la Biblia del Rey Jacobo, que influye significativamente en el idioma inglés. Antes de la Biblia del Rey Jacobo, muchas biblias en Inglaterra estaban en latín, lo que limitaba su acceso a los clérigos y a la élite educada. La traducción al inglés permitió que la Biblia fuera leída por un público mucho más amplio, haciendo del inglés la lengua dominante de la religión y la espiritualidad en Inglaterra</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://www.advocate.com/media-library/king-james-i-holy-bible-donald-trump-christian-version.jpg?id=51913589&width=1200&height=600&coordinates=0%2C0%2C0%2C78"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/8s3ZYerAcR8",
+                "caption": "Diccionario de Samuel Johnson",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1755"
+            },
+            "text": {
+                "headline": "Diccionario de Samuel Johnson",
+                "text": "<p>Se publica el Diccionario de la lengua inglesa de Samuel Johnson, uno de los primeros diccionarios de inglés moderno. Este diccionario ayudó a estandarizar la ortografía y la gramática del inglés, y sigue siendo una referencia importante para los estudiosos del idioma.</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://platform.vox.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/9271369/Samuel_Johnson_by_Joshua_Reynolds_2.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/C3ntt7BtP5Q",
+                "caption": "Expansion del Imperio Británico",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1837"
+            },
+            "end_date": {
+                "year": "1901"
+            },
+            "text": {
+                "headline": "Expansion del Imperio Británico",
+                "text": "<p>El Imperio Británico se expande a lo largo del siglo XIX, llevando el inglés a muchas partes del mundo. El inglés se convierte en un idioma global, hablado por millones de personas en todo el mundo. La expansión del imperio y la influencia cultural británica ayudaron a establecer el inglés como uno de los idiomas más importantes del mundo.</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://segredosdomundo.r7.com/wp-content/uploads/2022/10/10-curiosidades-surreais-sobre-a-era-vitoriana.jpg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/y_ZYsK9COjk",
+                "caption": "Diccionario de Oxford",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1884"
+            },
+            "text": {
+                "headline": "Diccionario de Oxford",
+                "text": "<p>Se publica la primera edición del Diccionario de Inglés de Oxford, una referencia importante para el idioma inglés.</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://media.assettype.com/freepressjournal/2023-01/f515737f-0023-4e49-b552-31ab6fb36e29/Untitled_design.jpeg"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/P7I2ZZachBA",
+                "caption": "Siglo XX",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1900"
+            },
+            "text": {
+                "headline": "Inglés como Lengua Internacional",
+                "text": "<p>El inglés se convierte en la lengua franca de la diplomacia, los negocios, la ciencia y la tecnología. El inglés es hablado por millones de personas en todo el mundo, y es una de las lenguas más importantes para la comunicación global"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://images.uncyclomedia.co/inciclopedia/es/thumb/a/a8/Siglo_XX.png/1200px-Siglo_XX.png"
+            }
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/embed/DmieF7YDp3g",
+                "caption": "Evolución del Inglés en la era digital",
+                "credit": ""
+            },
+            "start_date": {
+                "year": "1989"
+            },
+            "text": {
+                "headline": "Inglés contemporaneo y la Era Digital",
+                "text": "<p>El inglés se convierte en el idioma dominante en la era digital, con una gran cantidad de contenido en línea en inglés. El inglés es el idioma principal de la comunicación en línea, y es esencial para la participación en la economía digital y la sociedad global.</p>"
+            },
+            "background": {
+                "color": "#000000",
+                "url": "https://erbol.com.bo/sites/default/files/multitud_de_personas.jpg"
+            }
+        }
+    ]
+};
+
+window.timeline = new TL.Timeline('timeline-embed', timelineData);
